d33eec9713ab0695c36c012c9df1b2309cc38a47
obsesiva-syntaxis <obsesivasyntaxis@gmail.com>
2023-06-24 15:12:35 -0400

    bottom menu styles

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -5,13 +5,40 @@ const Menu = () => {
         <div className="menu">
             <div className="menu__logout">
                 <div className="menu__logout-btn">
-                    
+                    <i className="fas fa-power-off" />
                 </div>
             </div>
-            <div className="menu__navbar">
-                <i className="fa-solid fa-xmark"></i>
+            <div className="menu__nav">
+                <div className="menu__nav-bar">
+                    <div className="menu__nav-bar-item">
+                        <i className="fas fa-yin-yang"></i>
+                    </div>
+                    <div className="menu__nav-bar-item">
+                        <i className="fas fa-yin-yang"></i>
+                    </div>
+                    <div className="menu__nav-bar-item">
+                        <i className="fas fa-yin-yang"></i>
+                    </div>
+                    <div className="menu__nav-bar-item">
+                        <i className="fas fa-yin-yang"></i>
+                    </div>
+                    <div className="menu__nav-bar-item">
+                        <i className="fas fa-yin-yang"></i>
+                    </div>
+                    
+                </div>
+
             </div>
-            <div className="menu__user"></div>
+            <div className="menu__user">
+                <div className="menu__user-info">
+                    <div className="menu__user-info-name">Miguel Gonzalez Sierralta</div>
+                    <div className="menu__user-info-email">mgonzalez@ucclog.com</div>
+                </div>
+
+                <div className="menu__user-avatar">
+
+                </div>
+            </div>
         </div>
     )
 }
